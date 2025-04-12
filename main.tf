0c811e190225d3ae4a1b3ec6f5b55699073872d1
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-04-12 01:18:25 -0600

    feat: Import and managment already created  AWS Managment Console Infraestructure  into Terraform

--- main.tf
+++ main.tf
@@ -52,3 +52,19 @@ terraform {
     region = "us-east-1"
   }
 }
+
+# aws_instance.server-web:
+resource "aws_instance" "server-web" {
+    ami                                  = "ami-00a929b66ed6e0de6"
+    instance_type                        = "t2.micro"
+    vpc_security_group_ids               = [
+        "sg-0a8485d9019f060e6",
+    ]
+    tags = {
+    Name = "server-web"
+    Environment = "test"
+    Owner = "Fortino Romero"
+    Team = "devops"
+    Project = "server-web"
+  }
+}
