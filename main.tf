ac20a9a2d28849002664290fad04d32c8d599d72
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-04-11 20:31:19 -0600

    feat: Add security group and install nginx usign user_data

--- main.tf
+++ main.tf
@@ -11,5 +11,53 @@ resource "aws_instance" "nginx" {
   tags = {
     Name = "nginx-instance"
   }
+
+  // user_data is a script that runs on instance startup
+  // It installs and starts Nginx web server
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+            EOF
+  
+  key_name = aws_key_pair.nginx-ssh.key_name // Reference to the key pair for SSH access
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id] // Reference to the security group
+
+}
+
+resource "aws_key_pair" "nginx-ssh" {
+  key_name   = "nginx-ssh"
+  public_key = file("nginx-server.key.pub") 
+  
+}
+
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx-sg"
+  description = "Security group for Nginx server"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
 }
